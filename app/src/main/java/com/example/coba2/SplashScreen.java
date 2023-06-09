7e2a8eaed23d6901cb849d47fcaaf97021abd183
NekoKuroo <fedriann@gmail.com>
2023-06-09 20:34:02 +0700

    Initial commit

---
+++ app/src/main/java/com/example/coba2/SplashScreen.java
@@ -0,0 +1,28 @@
+package com.example.coba2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splashscreen);
+
+        View decorView = getWindow().getDecorView();
+        decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_FULLSCREEN);
+
+        new Handler().postDelayed(() -> {
+            Intent intent = new Intent(SplashScreen.this, MainActivity.class);
+            startActivity(intent);
+            finish();
+        }, 2000);
+
+    }
+}
